ab1a99a35c353850bb7d2ab84a3d91908a568ef6
lucascoelho5521-wq <lucascoelho5521@gmail.com>
2025-09-05 10:13:23 -0700

    add hello wolrd

---
+++ helloworld.js
@@ -0,0 +1,11 @@
+console.log("hello, world!");
+// ...existing code...
+for (let i = 0; i < 1001; i++) {
+    console.log("Loop iteration:", i);
+}
+// ...existing code...
+let count = 0;
+while (count < 1000000) {
+    console.log("While loop count:", count);
+    count++;
+}
